68bf98c80ed112f3780e63cba789ce771fcdb944
Danis Abadia <deanis89@hotmail.com>
2026-04-24 17:35:22 -0500

    chore: release v1.6.0

--- homebrew/Formula/sp.rb
+++ homebrew/Formula/sp.rb
@@ -1,26 +1,26 @@
 class Sp < Formula
   desc "Server management dashboard for Docker & Nginx"
   homepage "https://github.com/mrthoabby/serverpilot"
-  version "1.5.0"
+  version "1.6.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.0/sp-darwin-arm64"
-      sha256 "069dc96fb1c1dbd65f10c074657003188d169b40ebf6c53554c611b00633109c"
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.6.0/sp-darwin-arm64"
+      sha256 "ad377b18ab6cf667301bb539436725760459eeb1a38499e10897a533d607275e"
     else
-      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.0/sp-darwin-amd64"
-      sha256 "6df7e97023c67f903242ca075a2f7a4db44826ecd8e3c55a51c1128a69fe443e"
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.6.0/sp-darwin-amd64"
+      sha256 "7ce0bd28d46e98b12cc428b1dd99973c35d9af6ca832a26c3182926135ae2c94"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.0/sp-linux-arm64"
-      sha256 "7c39dd36ccd38015a6bb7fa792166b4d86e7ee8f2305f8faf8042022c53ee166"
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.6.0/sp-linux-arm64"
+      sha256 "dcba0de2dbd148cef0d104561d469171f2e45bf2fc06442039e1cc14014cc797"
     else
-      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.0/sp-linux-amd64"
-      sha256 "a8c87a95d783cab10b9f60bbd91c1b63bad68f7a758ce360fd71accd1c717e0e"
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.6.0/sp-linux-amd64"
+      sha256 "899ec950223e8f160c73a64f283c9f93837bc3b32717f59bf81a448a64392c1c"
     end
   end
 
---
+++ homebrew/Formula/sp@1.5.0.rb
@@ -0,0 +1,34 @@
+class SpAT1.5.0 < Formula
+  desc "Server management dashboard for Docker & Nginx"
+  homepage "https://github.com/mrthoabby/serverpilot"
+  version "1.5.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.0/sp-darwin-arm64"
+      sha256 "069dc96fb1c1dbd65f10c074657003188d169b40ebf6c53554c611b00633109c"
+    else
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.0/sp-darwin-amd64"
+      sha256 "6df7e97023c67f903242ca075a2f7a4db44826ecd8e3c55a51c1128a69fe443e"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.0/sp-linux-arm64"
+      sha256 "7c39dd36ccd38015a6bb7fa792166b4d86e7ee8f2305f8faf8042022c53ee166"
+    else
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.0/sp-linux-amd64"
+      sha256 "a8c87a95d783cab10b9f60bbd91c1b63bad68f7a758ce360fd71accd1c717e0e"
+    end
+  end
+
+  def install
+    bin.install Dir["sp-*"].first => "sp"
+  end
+
+  test do
+    system "#{bin}/sp", "version"
+  end
+end
