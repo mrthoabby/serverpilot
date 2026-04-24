ef2ee7c1725ec94a66fa6fd7f62d00643cf4baf5
Danis Abadia <deanis89@hotmail.com>
2026-04-24 14:23:19 -0500

    chore: release v2.2.0

--- homebrew/Formula/sp.rb
+++ homebrew/Formula/sp.rb
@@ -1,26 +1,26 @@
 class Sp < Formula
   desc "Server management dashboard for Docker & Nginx"
   homepage "https://github.com/mrthoabby/serverpilot"
-  version "1.0.0"
+  version "2.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mrthoabby/serverpilot/releases/download/v1.0.0/sp-darwin-arm64"
-      sha256 "0ac2960dcd1d2454ff1a50752fa55f566b550ad225f9d7fe894190236eb55bed"
+      url "https://github.com/mrthoabby/serverpilot/releases/download/v2.2.0/sp-darwin-arm64"
+      sha256 "35604b7211d34d3806703f039ca33aa0c73051f661b739be84ea927accf06f01"
     else
-      url "https://github.com/mrthoabby/serverpilot/releases/download/v1.0.0/sp-darwin-amd64"
-      sha256 "8be71caf0eabd13356442e3b2e5e68d15959b1c1fef2d9ad005299ae618e8674"
+      url "https://github.com/mrthoabby/serverpilot/releases/download/v2.2.0/sp-darwin-amd64"
+      sha256 "b4279f8b3cc611dcfde0875906e49f970b774b141761d18b8962b91efb035ca6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/mrthoabby/serverpilot/releases/download/v1.0.0/sp-linux-arm64"
-      sha256 "1a52654534361f254cfd59c007ea92113af327a7bbd1659e01b59ff8e50156ed"
+      url "https://github.com/mrthoabby/serverpilot/releases/download/v2.2.0/sp-linux-arm64"
+      sha256 "693ab74161399819a1826cf030693b8a462a2aad3a3a82e4abcf572dbd418314"
     else
-      url "https://github.com/mrthoabby/serverpilot/releases/download/v1.0.0/sp-linux-amd64"
-      sha256 "e11889228c350739ddf999fc6c90a28957d007f6ea4e33ae0437ffda3c83d82f"
+      url "https://github.com/mrthoabby/serverpilot/releases/download/v2.2.0/sp-linux-amd64"
+      sha256 "d0416b75b610d265b4781b31ece3879cf51b893cb19b93f6f8b048f4c9081a05"
     end
   end
 
---
+++ homebrew/Formula/sp@1.0.0.rb
@@ -0,0 +1,34 @@
+class SpAT1.0.0 < Formula
+  desc "Server management dashboard for Docker & Nginx"
+  homepage "https://github.com/mrthoabby/serverpilot"
+  version "1.0.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/mrthoabby/serverpilot/releases/download/v1.0.0/sp-darwin-arm64"
+      sha256 "0ac2960dcd1d2454ff1a50752fa55f566b550ad225f9d7fe894190236eb55bed"
+    else
+      url "https://github.com/mrthoabby/serverpilot/releases/download/v1.0.0/sp-darwin-amd64"
+      sha256 "8be71caf0eabd13356442e3b2e5e68d15959b1c1fef2d9ad005299ae618e8674"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/mrthoabby/serverpilot/releases/download/v1.0.0/sp-linux-arm64"
+      sha256 "1a52654534361f254cfd59c007ea92113af327a7bbd1659e01b59ff8e50156ed"
+    else
+      url "https://github.com/mrthoabby/serverpilot/releases/download/v1.0.0/sp-linux-amd64"
+      sha256 "e11889228c350739ddf999fc6c90a28957d007f6ea4e33ae0437ffda3c83d82f"
+    end
+  end
+
+  def install
+    bin.install Dir["sp-*"].first => "sp"
+  end
+
+  test do
+    system "#{bin}/sp", "version"
+  end
+end
