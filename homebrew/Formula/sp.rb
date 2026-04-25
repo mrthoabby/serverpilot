fc2dfac5a7e2ddd4d8d0cef3b225d87600db18ca
Danis Abadia <deanis89@hotmail.com>
2026-04-24 19:24:48 -0500

    chore: release v1.0.0

--- homebrew/Formula/sp.rb
+++ homebrew/Formula/sp.rb
@@ -7,20 +7,20 @@ class Sp < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.0.0/sp-darwin-arm64"
-      sha256 "ce49ba2e39436cc8e876b67706bc1ee77f685565e1ad5eebecfe1b42dce60fac"
+      sha256 "cfa9ea6d645f58fdb7faaf04c969a6d970f71b1955fbd85b51d1511afbf66f18"
     else
       url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.0.0/sp-darwin-amd64"
-      sha256 "10b1bff0ae54b64cf91313a76aaa01650021ee60bc694e867adb941a168a3fec"
+      sha256 "6adbc58a5e18b4d99680ba1047ae33a363d8f92409d8790df1d5cd8421594c03"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.0.0/sp-linux-arm64"
-      sha256 "a8ef539f8e9aa59ad6fa6ce2eda2a6a5cffad0991019d1a90dec5473370d94a5"
+      sha256 "6d51c182d307713ccdbf1916ab5aca2c514d910235f9accf24466112f2aec09e"
     else
       url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.0.0/sp-linux-amd64"
-      sha256 "1c3b9cc1b4247e44b1bd88dcda598c76939809169f348071359ea017ac9ab858"
+      sha256 "bc720188a1681c3261e7608528714f286283e3b424fc59d1a5c924ac39f0049e"
     end
   end
 
