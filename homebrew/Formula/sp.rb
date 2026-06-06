6959013f5ac144355419ad993377482b9175d651
Danis Abadia <deanis89@hotmail.com>
2026-06-06 18:10:36 -0500

    chore: release v1.5.10

--- homebrew/Formula/sp.rb
+++ homebrew/Formula/sp.rb
@@ -1,26 +1,26 @@
 class Sp < Formula
   desc "Server management dashboard for Docker & Nginx"
   homepage "https://github.com/mrthoabby/serverpilot"
-  version "1.5.9"
+  version "1.5.10"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.9/sp-darwin-arm64"
-      sha256 "c21b23994b8273c228812376a25ef857ed8bb6259554e05fb92e26c703e917c1"
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.10/sp-darwin-arm64"
+      sha256 "cd04bf525be0f9b97403e558559a7f80a2bcb83990f3d19638d39623cc9f402d"
     else
-      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.9/sp-darwin-amd64"
-      sha256 "bc9a7b5f9b4e2fcf45ca2139eba8ecec764def2c05a1844d9931e23234d56c46"
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.10/sp-darwin-amd64"
+      sha256 "8a0ace4d321f7399a6cd5698a97829cd6493fe2a94479be1819bbcf5915c3322"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.9/sp-linux-arm64"
-      sha256 "e96d81281ff314708e7f74375598b481ff20bd05dc76df6b0c2d947e98b42224"
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.10/sp-linux-arm64"
+      sha256 "6b0bb8c9b64dddafd9a0f953659c2ee4247f782a7128bf389f4ef7623ec755ed"
     else
-      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.9/sp-linux-amd64"
-      sha256 "b4635fb6e663fa1e712199743b4c2a5ef95661e18ee1ccdcb6b0fe2a44cf24de"
+      url "https://raw.githubusercontent.com/mrthoabby/serverpilot/master/release/1.5.10/sp-linux-amd64"
+      sha256 "c7f4e0bf1fffa72c3485e02273e186e4c60d6019676952ca0ac1e6c32104d069"
     end
   end
 
--- version.go
+++ version.go
@@ -1,3 +1,3 @@
 package main
 
-const Version = "1.5.9"
+const Version = "1.5.10"
